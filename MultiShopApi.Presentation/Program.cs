6d6ab94d9865a2716456888f3b9976fd8dc4f3bc
mmsmesut <m.mesutsincar@hotmail.com>
2024-07-04 23:01:35 +0300

    Swagger entegrasyonu yapıldı.

--- MultiShopApi.Presentation/Program.cs
+++ MultiShopApi.Presentation/Program.cs
@@ -3,7 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 //1.Adým MVC yapýsýný dahil et
 builder.Services.AddControllers();//Asp.Net Core MVC altyapýsýný uygulamamýza dahil etmemizi saðlar
 
+//Swagger 1
+builder.Services.AddEndpointsApiExplorer(); //Swaggerin apiyi incelemesi ve belgelerin oluþturulmasý 
+builder.Services.AddSwaggerGen();//Swagger belgeleri için gerekli tüm ayarlarýn yapýlandýrýlmasý
+
 var app = builder.Build();
+
+//Swagger 2
+if (app.Environment.IsDevelopment())//Swagger'i Development sadecce ortamýnda aktifleþtirir
+{
+    app.UseSwagger();  //Apilerin yaptýðý iþlemleri ve bu iþlemlerin nasýl kullanýlacaðýný açýklayan metadalardýr 
+    app.UseSwaggerUI();//Apilerin görsel ayarlamalarý'ný aktif hale getirir
+}
 
 //2.Adým MVC Controller'a Yönlendirme
 app.MapControllers(); //Asp.Net Core uygulamasýndaki http isteklerini Api'nin Controller'ýna yönelerimeyi saðlar
